aaf493ad97083775ce5d026d256741b8c3e78f9a
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-07 00:07:48 -0200

    Improves last exercise implementation

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ new Vue({
         methods: {
         	alert: function() {
           	alert('Button was clicked!')
-          },
-          storeValue: function() {
-          	this.value = event.target.value;
-          },
-          updateValue: function() {
-          	this.value = event.target.value;
           }
         }
     });
