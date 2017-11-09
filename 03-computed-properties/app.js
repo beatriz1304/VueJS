9f7acb46daa713e58a4635e8d51a77dcccded915
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-09 20:24:21 -0200

    Implements computed properties

---
+++ 03-computed-properties/app.js
@@ -0,0 +1,19 @@
+new Vue({
+	el: '#app',
+  data: {
+  	counter: 0,
+    secondCounter: 0
+  },
+  computed: {
+  	output: function() {
+    	console.log('Computed');
+    	return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
+    }
+  },
+  methods: {
+  	result() {
+      console.log('Method');
+    	return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
+    }
+  }
+})
