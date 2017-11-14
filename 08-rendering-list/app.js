218d176293dcd519e68d0d7aba1ee14106cb8be6
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-14 19:16:13 -0200

    Implements rendering list with v-for

---
+++ 08-rendering-list/app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+    	{name: 'Max', age: 27, color: 'red'},
+      {name: 'Anna', age: 'unknown', color: 'blue'}
+    ]
+  }
+});
