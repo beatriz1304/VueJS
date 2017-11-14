31987242051e117f473b5afaa1e7e806137a13cf
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-14 19:34:17 -0200

    Implements looping through objects

---
+++ 09-looping-object/app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [{
+      name: 'Bia',
+      age: 27,
+      color: 'red'
+    }, {
+      name: 'Anna',
+      age: 'unknown',
+      color: 'blue'
+    }]
+  }
+});
