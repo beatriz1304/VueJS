d13b9bafbca7ca4c5d1ce10acfa30bacec03248e
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-17 22:35:12 -0200

    Implements first component

---
+++ 11-Component/app.js
@@ -0,0 +1,12 @@
+Vue.component('my-cmp', {
+	data: function() {
+  	return {
+    	status: 'Critical'
+    }
+  },
+  template: '<p>Server Status: {{ status }}</p>'
+});
+
+new Vue({
+	el: '#app'
+})
