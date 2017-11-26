556699650e768c8b531b5a2258050c5c2e6e54ba
Beatriz da Silveira <beatriz1304@gmail.com>
2017-11-26 11:29:53 -0200

    Implements communication between sibling components

---
+++ 12-component/src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: {
+                type: String
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.myName;
+            },
+            resetName() {
+                this.myName = 'Beatriz';
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
